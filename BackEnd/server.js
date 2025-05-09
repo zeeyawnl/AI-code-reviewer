212290663d63aa1c4f21b17b60242bb28791b930
zeeya <notzeeya@gmail.com>
2025-05-09 15:17:44 +0530

    fix:use dynaic port for deployment

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const app = require('./src/app')
 
 
-app.listen(3000, () => {
-    console.log(`server has started on http://localhost:3000`)
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
